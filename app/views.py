560af8b0ee8c89a83f172f492bb2ed930ae38287
bellahOchola <bellahkenya@gmail.com>
2019-11-17 14:48:51 +0300

    install flask-boostrap

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
-# from flask import render_template
-# from app import app
-# from .request import get_news
+from flask import render_template
+from app import app
+from .request import get_news
 
 
 @app.route('/')
