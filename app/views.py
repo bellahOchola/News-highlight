08d59dd8db6cf52f3d22ba1ff7d9621d439098e0
bellahOchola <bellahkenya@gmail.com>
2019-11-19 11:57:13 +0300

    create class articles

--- app/views.py
+++ app/views.py
@@ -13,12 +13,12 @@ def index():
     general_news = get_news('general')
     return render_template('index.html', general = general_news)
 
-@app.route('/articles/<id>')
-def articles(id):
-    '''
-    returns the articles page and its data
-    '''
-    news_articles = get_articles(id)
-    id = id
-    print(news_articles)
-    return render_template('articles.html',artiq = news_articles, id = id)
+# @app.route('/articles/<id>')
+# def articles(id):
+#     '''
+#     returns the articles page and its data
+#     '''
+#     news_articles = get_articles(id)
+#     id = id
+#     print(news_articles)
+#     return render_template('articles.html',artiq = news_articles, id = id)
